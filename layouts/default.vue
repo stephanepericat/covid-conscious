1223e769f9d392b8afa10fdb502bf3dbfb762add
Stephane P. Pericat <shortwavaudio@gmail.com>
2023-05-28 11:54:41 -0500

    logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,48 @@
 <template>
   <ILayout class="default-layout">
-    <ILayoutHeader> Header </ILayoutHeader>
+    <ILayoutHeader>
+      <INavbar color="light">
+        <INavbarBrand to="/">
+          <img  class="default-layout__logo" src="/covid-conscious-logo.svg" />
+        </INavbarBrand>
+        <INavbarCollapsible>
+            <INav>
+                <INavItem to="/"> Home </INavItem>
+                <INavItem to="/about"> About </INavItem>
+                <INavItem to="/contact"> Contact </INavItem>
+            </INav>
+            <!-- <IInput placeholder="Type something..">
+                <template #append>
+                    <IButton color="primary">
+                        <IIcon name="ink-search" />
+                    </IButton>
+                </template>
+            </IInput> -->
+        </INavbarCollapsible>
+    </INavbar>
+    </ILayoutHeader>
     <ILayout vertical>
         <ILayoutContent>
           <NuxtPage />
         </ILayoutContent>
         <ILayoutAside> Right Aside </ILayoutAside>
     </ILayout>
-    <ILayoutFooter> Footer </ILayoutFooter>
+    <ILayoutFooter>
+      <div class="default-layout__footer">&nbsp;</div>
+    </ILayoutFooter>
   </ILayout>
 </template>
 <style lang="scss" scoped>
   .default-layout {
     height: 100vh;
+
+    &__logo {
+      height: 60px;
+      width: auto;
+    }
+
+    &__footer {
+      background-color: var(--color-light);
+    }
   }
 </style>
