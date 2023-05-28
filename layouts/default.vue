6caecff9b4c158bbb6aeabf415cdba387a10b622
Stephane P. Pericat <shortwavaudio@gmail.com>
2023-05-28 11:38:07 -0500

    default layout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <ILayout class="default-layout">
+    <ILayoutHeader> Header </ILayoutHeader>
+    <ILayout vertical>
+        <ILayoutContent>
+          <NuxtPage />
+        </ILayoutContent>
+        <ILayoutAside> Right Aside </ILayoutAside>
+    </ILayout>
+    <ILayoutFooter> Footer </ILayoutFooter>
+  </ILayout>
+</template>
+<style lang="scss" scoped>
+  .default-layout {
+    height: 100vh;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>{{ title }} home page</div>
+</template>
+<script setup>
+const title = ref("cccc.org");
+</script>
