b6e890c01ece2a60d09c174511c35361338cafa7
Stephane P. Pericat <shortwavaudio@gmail.com>
2023-06-07 23:11:27 -0500

    get feed data

--- server/api/feed.js
+++ server/api/feed.js
@@ -1,16 +1,28 @@
-export default eventHandler((event) => {
+import rssFeedQuery from "../../sanity/rssFeed.sanity";
+
+export default eventHandler(async (event) => {
+  const { fetch } = useSanity();
+
   const query = getQuery(event);
   const locale = query?.lang || "en";
+
   const { origin } = getRequestURL(event);
-  console.log("info", {locale, origin});
 
-  setHeader(event, "Content-Type", "application/xml");
+  try {
+    const { entries, settings } = await fetch(rssFeedQuery, { locale });
+    // console.log("settings", settings);
+    // console.log("entries", entries);
 
-  return `<?xml version="1.0" encoding="UTF-8"?>
-    <note>
-      <to>Tove</to>
-      <from>Jani</from>
-      <heading>Reminder</heading>
-      <body>Don't forget me this weekend!</body>
-    </note>`;
+    setHeader(event, "Content-Type", "application/xml");
+
+    return `<?xml version="1.0" encoding="UTF-8"?>
+      <note>
+        <to>Tove</to>
+        <from>Jani</from>
+        <heading>Reminder</heading>
+        <body>Don't forget me this weekend!</body>
+      </note>`;
+  } catch(e) {
+    console.log("error", e.message);
+  }
 });
