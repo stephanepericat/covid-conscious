0aafbe754cded17302fd85a540089533a1e3a718
Stephane P. Pericat <shortwavaudio@gmail.com>
2023-06-15 21:43:55 -0500

    parse body

---
+++ assets/utils/body-parser.js
@@ -0,0 +1,7 @@
+export const parseBody = (body) => Object.keys(body).reduce((acc, val) => {
+  if(body[val].value) {
+    acc[val] = body[val].value || null;
+  }
+
+  return acc;
+}, {});
--- server/api/contribute.post.js
+++ server/api/contribute.post.js
@@ -1,7 +1,10 @@
+import { parseBody } from "../../assets/utils/body-parser";
+
 export default eventHandler(async (event) => {
   const body = await readBody(event);
 
-  console.log("body", body);
+  const parsed = parseBody(body);
+  console.log("body", parsed);
 
   return { ok: true };
 });
