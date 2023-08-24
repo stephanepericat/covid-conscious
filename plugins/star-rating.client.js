ca42d370e63f018ad9496260ba30453b19e4c0db
Stephane P. Pericat <shortwavaudio@gmail.com>
2023-08-24 17:38:05 -0500

    fix star rating loading

---
+++ plugins/star-rating.client.js
@@ -0,0 +1,8 @@
+// import { QuillEditor } from '@vueup/vue-quill'
+import StarRating from 'vue-star-rating'
+import consola from 'consola'
+
+export default defineNuxtPlugin(({ vueApp }) => {
+  consola.info('Setting up star rating...')
+  vueApp.component('StarRating', StarRating)
+})
