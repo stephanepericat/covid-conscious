407caf57b2140e1c2a42ada596980171b6c13cd9
Stephane P. Pericat <shortwavaudio@gmail.com>
2023-09-06 14:28:11 -0500

    terms and conditions

--- pages/disclaimer.vue
+++ pages/disclaimer.vue
@@ -2,37 +2,21 @@
   <div class="disclaimer-page" :class="{ pending }">
     <ILoader v-if="pending" class="disclaimer-page__loader" />
     <template v-else>
-      <h1 class="disclaimer-page__title" v-text="data.title" />
-      <em class="disclaimer-page__update">
-        {{ $t('disclaimer.updated') }}: {{ format(new Date(data.updated), DEFAULT_DATE_FORMAT) }}
-      </em>
-      <section>
-        <h2 v-text="$t('disclaimer.website')" />
-        <SanityContent :blocks="data.website" />
-      </section>
-      <section>
-        <h2 v-text="$t('disclaimer.links')" />
-        <SanityContent :blocks="data.links" />
-      </section>
-      <section>
-        <h2 v-text="$t('disclaimer.professional')" />
-        <SanityContent :blocks="data.professional" />
-      </section>
-      <section>
-        <h2 v-text="$t('disclaimer.testimonials')" />
-        <SanityContent :blocks="data.testimonials" />
-      </section>
+      <h1 class="disclaimer-page__title">
+        {{ policy[0].title }}
+      </h1>
+      
+      <div class="disclaimer-page__contents">
+        <SanityContent :blocks="policy[0].contents" />
+      </div>
     </template>
   </div>
 </template>
 <script setup>
-  import { format } from "date-fns";
-  import disclaimerQuery from "~/sanity/disclaimer.sanity";
-  import { DEFAULT_DATE_FORMAT } from "~/assets/constants/date-formats";
+  import policyQuery from '~/sanity/policy.sanity'
 
-  const { locale } = useI18n();
-
-  const { data, pending } = useLazySanityQuery(disclaimerQuery, { locale });
+  const { locale } = useI18n()
+  const { data: policy, pending } = useLazySanityQuery(policyQuery, { locale, policyType: "Disclaimer" })
 </script>
 <style lang="scss" scoped>
 @import "~/assets/sass/mixins.scss";
@@ -48,11 +32,6 @@
 
   &__title {
     @include title();
-  }
-
-  &__update {
-    display: block;
-    margin-bottom: 40px;
   }
 }
 </style>
--- pages/terms-conditions.vue
+++ pages/terms-conditions.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="privacy-policy-page" :class="{ pending }">
-    <ILoader v-if="pending" class="privacy-policy-page__loader" />
+  <div class="terms-conditions-page" :class="{ pending }">
+    <ILoader v-if="pending" class="terms-conditions-page__loader" />
     <template v-else>
-      <h1 class="privacy-policy-page__title">
+      <h1 class="terms-conditions-page__title">
         {{ policy[0].title }}
       </h1>
       
-      <div class="privacy-policy-page__contents">
+      <div class="terms-conditions-page__contents">
         <SanityContent :blocks="policy[0].contents" />
       </div>
     </template>
@@ -16,12 +16,12 @@
   import policyQuery from '~/sanity/policy.sanity'
 
   const { locale } = useI18n()
-  const { data: policy, pending } = useLazySanityQuery(policyQuery, { locale, policyType: "Privacy Policy" })
+  const { data: policy, pending } = useLazySanityQuery(policyQuery, { locale, policyType: "Terms & Conditions" })
 </script>
 <style lang="scss" scoped>
 @import "~/assets/sass/mixins.scss";
 
-.privacy-policy-page {
+.terms-conditions-page {
   &.pending {
     @include pending();
   }
