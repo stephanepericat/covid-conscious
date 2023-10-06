111a9cc6ff9a8199c799f373c8dcd105d57c56cb
Stephane P. Pericat <shortwavaudio@gmail.com>
2023-10-05 21:30:13 -0500

    simplify api

---
+++ server/api/translate.post.js
@@ -0,0 +1,39 @@
+import { Translator } from 'deepl-node'
+import consola from 'consola'
+
+export default eventHandler(async (event) => {
+  const { locale = null, text = null } = await readBody(event)
+
+  if(!locale) {
+    sendError(
+      event,
+      createError({ statusCode: 400, statusMessage: 'No locale specified' })
+    )
+  }
+
+  if(!text) {
+    sendError(
+      event,
+      createError({ statusCode: 400, statusMessage: 'No text supplied' })
+    )
+  }
+
+  const d = new Translator(process.env.DEEPL_API_KEY)
+  const usage = await d.getUsage()
+
+  consola.info(`[${Date.parse(new Date())}] DeepL usage: ${JSON.stringify(usage.character)}`)
+
+  if(usage.anyLimitReached()) {
+    sendError(
+      event,
+      createError({ statusCode: 429, statusMessage: 'Too many requests' })
+    )
+  }
+
+  try {
+    return await d.translateText(text, null, locale)
+  } catch(e) {
+    console.error(e?.text || null, body)
+    sendError(event, e)
+  }
+})
