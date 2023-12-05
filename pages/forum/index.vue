da2c178bc4e7c532c757b549361cec33d0b41ad9
Stephane P. Pericat <shortwavaudio@gmail.com>
2023-12-05 11:06:23 -0600

    forum user actions

--- pages/forum/index.vue
+++ pages/forum/index.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="sf-index-page">
-    <h1
-      class="sf-index-page__title"
-      v-text="$t('forum.latest.title')"
-    />
+    <div class="sf-index-page__header">
+      <h1
+        class="sf-index-page__title"
+        v-text="$t('forum.latest.title')"
+      />
+      <div v-if="isLoggedIn" class="sf-index-page__header--actions">
+        <IButton class="sf-index-page__header--actions-button" :to="localePath(`/${FORUM}/my-posts`)">
+          <template #icon>
+            <Icon name="material-symbols:folder-managed-outline-rounded" />
+          </template>
+          {{ $t('forum.nav.my.posts') }}
+        </IButton>
+        <IButton class="sf-index-page__header--actions-button" :to="localePath(`/${FORUM}/create`)">
+          <template #icon>
+            <Icon name="material-symbols:ink-pen-rounded" />
+          </template>
+          {{ $t('forum.nav.create') }}</IButton>
+      </div>
+    </div>
     <PostList
       :pending="loading"
       :posts="posts"
@@ -15,6 +30,10 @@
 <script setup>
   import PostList from '~/components/PostList.vue'
   import { usePosts } from '~/assets/composables/usePosts'
+  import { FORUM } from '~/assets/constants/types'
+
+  const user = useSupabaseUser()
+  const isLoggedIn = computed(() => !!user?.value?.id)
 
   const {
     getPosts,
@@ -36,6 +55,20 @@
 @import "~/assets/sass/mixins.scss";
 
 .sf-index-page {
+  &__header {
+    display: flex;
+    justify-content: space-between;
+
+    &--actions {
+      align-items: center;
+      display: flex;
+
+      &-button {
+        margin-left: 10px;
+      }
+    }
+  }
+  
   &__title {
     @include title();
   }
