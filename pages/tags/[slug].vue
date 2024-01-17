5f099dd215b2f64aae38b8f67102941fcc8702ab
Stephane P. Pericat <shortwavaudio@gmail.com>
2024-01-16 19:04:07 -0600

    add tags route

---
+++ pages/tags/[slug].vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="tags-page">
+    {{ slug }}
+  </div>
+</template>
+<script setup>
+  const route = useRoute()
+  const slug = computed(() => route.params.slug)
+</script>
+<style lang="scss" scoped>
+</style>
