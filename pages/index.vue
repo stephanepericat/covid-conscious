f43d3ce9c41f4e5ff54fbb91416b4a2894923ecb
Stephane P. Pericat <shortwavaudio@gmail.com>
2024-05-22 20:21:31 -0500

    rebuild forum section

--- pages/index.vue
+++ pages/index.vue
@@ -6,23 +6,31 @@
         <div>
           TOP
         </div>
-        <!-- <div class="home-page__divider" /> -->
+        <div class="home-page__divider" />
         <div>
-          <h3>{{ $t("layout.forum") }}</h3>
+          <h3 class="uppercase font-base font-bold text-base tracking-widest hover:no-underline mt-4">
+            {{ $t("layout.forum") }}
+          </h3>
           <template v-if="posts?.length">
-            <div>
+            <div class="grid lg:grid-cols-3 gap-4 lg:gap-8">
               <PostPreview
                 v-for="post in posts"
                 :key="post.id"
+                hide-thumbnail
                 :post="post"
                 :root-path="rootPath"
                 :ssr="isSSR"
                 with-preview
               />
             </div>
-            <NuxtLink :to="localePath('/forum')">
-              {{ $t('layout.more.posts') }} &raquo;
-            </NuxtLink>
+            <div class="mt-4">
+              <NuxtLink
+                :to="localePath('/forum')"
+                class="uppercase font-semibold text-xs tracking-widest hover:no-underline"
+              >
+                {{ $t('layout.more.posts') }} &raquo;
+              </NuxtLink>
+            </div>
           </template>
           <p v-else>{{ $t('layout.empty.forum') }}</p>
         </div>
@@ -71,8 +79,8 @@
     @include loader();
   }
 
-  // &__divider {
-  //   @include divider();
-  // }
+  &__divider {
+    @include divider();
+  }
 }
 </style>
