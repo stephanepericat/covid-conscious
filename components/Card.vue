d69d0b4574556a7d176f926c8a3cda1daab5480f
Stephane P. Pericat <shortwavaudio@gmail.com>
2024-05-26 17:15:45 -0500

    defaults

---
+++ components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">card</div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss">
+@import "~/assets/sass/mixins.scss";
+
+.card {
+  @include lightBg();
+}
+
+.dark-theme {
+  .card {
+    @include darkBg();
+  }
+}
+</style>
