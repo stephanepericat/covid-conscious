276b0bc764f95bd2a902976972c51d886639fa57
Stephane Pericat <shortwavlabs@StephanesLaptop.attlocal.net>
2024-09-01 18:29:58 -0500

    tailwind them extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   darkMode: ['selector', '[class="dark-theme"]'],
   // ...
+  theme: {
+    fontFamily: {
+      'title': ['Oswald']
+    },
+    letterSpacing: {
+      'title': '0.05rem',
+    },
+  }
 }
