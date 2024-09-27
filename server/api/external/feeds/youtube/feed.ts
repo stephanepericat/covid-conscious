433cc9176d269992df3161f0e1f4dc694c6cb959
Stephane Pericat <shortwavlabs@StephanesLaptop.attlocal.net>
2024-09-27 15:06:07 -0500

    get feed entries

--- server/api/external/feeds/youtube/feed.ts
+++ server/api/external/feeds/youtube/feed.ts
@@ -1,11 +1,32 @@
-import consola from "consola";
-import { Feed } from "feed";
+import consola from 'consola';
+import { Feed } from 'feed';
 import { parseStringPromise } from 'xml2js'
+import _ from 'lodash'
 
-import ytFeedQuery from "~/sanity/ytFeed.sanity";
+import ytFeedQuery from '~/sanity/ytFeed.sanity'
 
 type YtFeed = {
   feedURL: string;
+}
+
+type Entry = {
+  author: string;
+  description: string;
+  link: string;
+  publicationDate: Date;
+  title: string;
+  thumbnail: string;
+}
+
+const mapEntries = (entry: any): Entry => {
+  return {
+    author: entry.author[0].name[0],
+    description: entry['media:group'][0]['media:description'][0].split('').slice(0, 252).join('') + '...',
+    link: entry.link[0].$.href,
+    publicationDate: new Date(entry.published[0]),
+    title: entry.title[0],
+    thumbnail: entry['media:group'][0]['media:thumbnail'][0].$.url,
+  }
 }
 
 export default defineEventHandler(async (event) => {
@@ -18,13 +39,16 @@ export default defineEventHandler(async (event) => {
         const data = await fetch(feedURL);
         const xml = await data.text()
         const { feed } = await parseStringPromise(xml)
-        return feed
+        return feed.entry.map(mapEntries)
       } catch(e) {
+        consola.error(e)
         return {}
       }
     })
     const feeds = await Promise.all(calls)
-    console.log(feeds)
+    const sorted = _.orderBy(feeds.flat(), 'publicationDate', 'desc')
+    const feedEntries = sorted.slice(0, 20);
+    console.log('feedEntries', feedEntries, feedEntries.length)
   } catch(e) {
     consola.error(e)
     return null
