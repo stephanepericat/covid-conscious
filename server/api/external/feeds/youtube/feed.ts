6fb351305c063e9e329db000b9f305069cde9bcc
Stephane Pericat <shortwavlabs@StephanesLaptop.attlocal.net>
2024-09-28 11:06:01 -0500

    videos feed

--- server/api/external/feeds/youtube/feed.ts
+++ server/api/external/feeds/youtube/feed.ts
@@ -10,8 +10,12 @@ type YtFeed = {
 }
 
 type Entry = {
-  author: string;
+  author: {
+    link: string;
+    name: string;
+  };
   description: string;
+  id: string,
   link: string;
   publicationDate: Date;
   title: string;
@@ -20,8 +24,12 @@ type Entry = {
 
 const mapEntries = (entry: any): Entry => {
   return {
-    author: entry.author[0].name[0],
+    author: {
+      link: entry.author[0].uri[0],
+      name: entry.author[0].name[0],
+    },
     description: entry['media:group'][0]['media:description'][0].split('').slice(0, 252).join('') + '...',
+    id: entry.id[0],
     link: entry.link[0].$.href,
     publicationDate: new Date(entry.published[0]),
     title: entry.title[0],
@@ -31,6 +39,7 @@ const mapEntries = (entry: any): Entry => {
 
 export default defineEventHandler(async (event) => {
   const { fetch: sanityFetch } = useSanity()
+  const { origin: BASE_URL } = getRequestURL(event)
 
   try {
     const feedUrls: YtFeed[] = await sanityFetch(ytFeedQuery)
@@ -47,12 +56,48 @@ export default defineEventHandler(async (event) => {
     })
     const feeds = await Promise.all(calls)
     const sorted = _.orderBy(feeds.flat(), 'publicationDate', 'desc')
-    const feedEntries = sorted.slice(0, 20);
-    console.log('feedEntries', feedEntries, feedEntries.length)
+    const feedEntries = sorted.slice(0, 20)
+    
+    const feed = new Feed({
+      title: 'Covidnet Feed',
+      description: 'RSS Feed for Covidnet videos',
+      id: `${BASE_URL}/`,
+      link: `${BASE_URL}/`,
+      language: 'en',
+      image: 'https://cdn.sanity.io/images/yt0dcu6v/production/def76fe97d0b33358120bd7da553abba52a939d2-1080x1080.jpg',
+      favicon: BASE_URL + "/favicon.ico",
+      copyright: `All rights reserved ${new Date().getFullYear()}, That Covid Life.`,
+      updated: feedEntries?.[0] ? new Date(feedEntries[0]?.publicationDate) : undefined,
+      author: {
+        email: 'contact@thatcovid.life',
+        link: BASE_URL,
+        name: 'That Covid Life',
+      },
+    })
+
+    feedEntries.forEach((entry) => {
+      feed.addItem({
+        title: entry.title,
+        description: entry.description || '',
+        published: entry.publicationDate,
+        id: entry.id,
+        link: entry.link,
+        date: entry.publicationDate,
+        image: entry.thumbnail,
+        author: [entry.author],
+        category: [
+          { name: 'YouTube' },
+          { name: 'Video' },
+          { name: 'Covid-19' }
+        ],
+      })
+    })
+
+    setHeader(event, 'Content-Type', 'application/xml')
+
+    return feed.rss2()
   } catch(e) {
     consola.error(e)
     return null
   }
-
-  return {ok: true }
 })
