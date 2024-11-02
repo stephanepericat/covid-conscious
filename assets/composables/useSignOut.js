d69a026615674a9fda8d23ca6b713b6b4445ae4c
Stephane Pericat <shortwavlabs@StephanesLaptop.attlocal.net>
2024-11-02 12:53:21 -0500

    logout

--- assets/composables/useSignOut.js
+++ assets/composables/useSignOut.js
@@ -3,13 +3,14 @@ import { useToast } from '@inkline/inkline';
 export const useSignOut = (user) => {
   const localePath = useLocalePath()
   const router = useRouter()
-  const supabase = useSupabaseClient()
+  // const supabase = useSupabaseClient()
   const toast = useToast()
   const { t } = useI18n()
+  const { clear } = useUserSession()
 
   const onLogoutSuccess = () => {
     user.value = null
-    router.push(localePath('/login'))
+    router.push(localePath('/'))
   }
 
   const onError = () => {
@@ -22,8 +23,9 @@ export const useSignOut = (user) => {
 
   const signOut = async () => {
     try {
-      let { error } = await supabase.auth.signOut()
-      if (error) throw error
+      // let { error } = await supabase.auth.signOut()
+      // if (error) throw error
+      await clear()
       onLogoutSuccess()
     } catch (error) {
       onError()
