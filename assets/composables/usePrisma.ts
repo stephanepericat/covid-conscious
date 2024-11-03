0508060d878b687ed59312a49c749f9d6baee3fd
Stephane Pericat <shortwavlabs@StephanesLaptop.attlocal.net>
2024-11-03 12:18:46 -0600

    auth.d.ts

--- assets/composables/usePrisma.ts
+++ assets/composables/usePrisma.ts
@@ -1,14 +1,15 @@
 import consola from 'consola'
 
 export const usePrisma = () => {
-  const getUser = async (email: string): Promise<any> => {
+  const getUser = async (email: string) => {
     try {
-      const { data } = 
+      const { data, error } = 
         useFetch('/api/forum/user/get-user', {
           method: 'POST',
           body: { email }
         })
-      return data
+      if(error.value) throw error.value
+      return data.value || null
     } catch(e) {
       consola.error(e)
       return null
--- server/api/forum/user/get-user.post.ts
+++ server/api/forum/user/get-user.post.ts
@@ -2,7 +2,24 @@ import consola from 'consola'
 import prisma from '~/lib/prisma'
 
 export default defineEventHandler(async (event) => {
+  const { user } = await getUserSession(event)
   const { email } = await readBody(event)
+
+  if(!email) {
+    throw createError({
+      status: 400,
+      message: "Bad request",
+      statusMessage: "Email is missing",
+    });
+  }
+
+  if(!user || user.email !== email) {
+    throw createError({
+      status: 403,
+      message: "Unauthorized",
+      statusMessage: "You are not authorized to access this resource",
+    });
+  }
 
   try {
     return await prisma.user.findUniqueOrThrow({
