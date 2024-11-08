2e73a1b7236f0e938f71d64f8326ddd056786faf
Stephane Pericat <shortwavlabs@StephanesLaptop.attlocal.net>
2024-11-07 23:13:19 -0600

    mock cleanup

--- tests/mocks/prisma.js
+++ tests/mocks/prisma.js
@@ -1,15 +1,1 @@
-// import { PrismaClient as BasePrismaClient } from '@prisma/client'
-// import { beforeEach } from 'vitest'
-// import { mockDeep /*, mockReset */ } from 'vitest-mock-extended'
-
-// beforeEach(() => {
-//   mockReset(prismaMock)
-// })
-
-// const PrismaClient = mockDeep(BasePrismaClient)
-
-class PrismaClient {
-
-}
-
-export { PrismaClient }
+export class PrismaClient {}
