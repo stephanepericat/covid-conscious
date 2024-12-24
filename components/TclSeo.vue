5499f297a5bfa0f321ae750ff3e5b8e756279bd4
Stephane Pericat <shortwavlabs@shortwavlabs.local>
2024-12-24 14:22:32 -0600

    seo component

---
+++ components/TclSeo.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps({
+  description: { type: String, required: true },
+  image: { type: String, default: null },
+  imageType: { type: String, default: null },
+  title: { type: String, required: true },
+})
+
+const { locale } = useI18n()
+</script>
+
+<template>
+  <Head>
+    <Title>{{ title }}</Title>
+    <Meta name="description" :content="description" />
+    <Meta name="og:title" :content="title" />
+    <Meta name="og:description" :content="description" />
+    <Meta v-if="image" name="og:image" :content="image" />
+    <Meta v-if="image" name="og:image:secure_url" :content="image" />
+    <Meta v-if="imageType" name="og:image:type" :content="imageType" />
+    <Meta v-if="image" name="og:image:alt" :content="title" />
+    <Meta name="og:locale" :content="locale" />
+    <Meta name="og:site_name" :content="$t('layout.tcl')" />
+    <Meta name="twitter:title" :content="title" />
+    <Meta name="twitter:description" :content="description" />
+    <Meta v-if="image" name="twitter:image" :content="image" />
+    <Meta name="twitter:site" content="@thatcovidlife" />
+    <Meta name="twitter:card" content="summary_large_image" />
+  </Head>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,18 @@
 <script setup lang="ts">
-const { locale } = useI18n()
+const host = computed(() => window?.location?.origin || '')
 </script>
 
 <template>
   <div class="home-page">
-    <Head>
-      <Title>{{ $t('layout.home') }}</Title>
-      <Meta name="description" :content="$t('layout.description')" />
-      <Meta name="og:title" :content="$t('layout.home')" />
-      <Meta name="og:description" :content="$t('layout.description')" />
-      <Meta name="og:image" content="/tcl-fallback-169.jpg" />
-      <Meta name="og:image:secure_url" content="/tcl-fallback-169.jpg" />
-      <Meta name="og:image:type" content="image/jpeg" />
-      <Meta name="og:image:alt" :content="$t('layout.home')" />
-      <Meta name="og:locale" :content="locale" />
-      <Meta name="og:site_name" :content="$t('layout.tcl')" />
-      <Meta name="twitter:title" :content="$t('layout.home')" />
-      <Meta name="twitter:description" :content="$t('layout.description')" />
-      <Meta name="twitter:image" content="/tcl-fallback-169.jpg" />
-      <Meta name="twitter:site" content="@thatcovidlife" />
-      <Meta name="twitter:card" content="summary_large_image" />
-    </Head>
+    <TclSeo
+      :description="$t('layout.description')"
+      :image="`${host}/tcl-fallback-169.jpg`"
+      image-type="image/jpeg"
+      :title="$t('layout.home')"
+    />
 
     <h1 class="font-title text-5xl font-bold uppercase">
-      Home Page {{ $t('layout.tcl') }}
+      {{ $t('layout.tcl') }}
     </h1>
     <Button>Click me!</Button>
   </div>
