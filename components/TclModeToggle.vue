f3513165dc5a8ca275f9ff41a067a63ba6f2385e
Stephane Pericat <shortwavlabs@Mac.attlocal.net>
2024-12-25 15:26:31 -0600

    fix icon styles

--- components/TclModeToggle.vue
+++ components/TclModeToggle.vue
@@ -7,11 +7,11 @@ const colorMode = useColorMode()
     <DropdownMenuTrigger>
       <Button variant="outline" size="icon" class="rounded-full">
         <Icon
-          name="line-md:sunny"
+          name="bi:sun"
           class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
         />
         <Icon
-          name="line-md:moon"
+          name="mynaui:moon-star"
           class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
         />
         <span class="sr-only">{{ $t('layout.toggleTheme') }}</span>
