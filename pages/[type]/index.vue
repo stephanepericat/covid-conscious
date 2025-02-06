17360a39060a5a0354ba97e40543e508fe3355f0
Stephane Pericat <shortwavlabs@Mac.attlocal.net>
2025-02-06 17:48:02 -0600

    feat(tcl-207): pagination

--- pages/[type]/index.vue
+++ pages/[type]/index.vue
@@ -1,6 +1,29 @@
 <script setup lang="ts">
+const localePath = useLocalePath()
 const route = useRoute()
+const router = useRouter()
+
 const type = computed(() => route.params.type || null)
+const limit = computed(() => parseInt(route.query?.limit as string) || 5)
+const offset = computed(() => parseInt(route.query?.offset as string) || 0)
+
+const onPreviousPage = (e: Event) => {
+  e.preventDefault()
+
+  if (offset.value === 0) {
+    return
+  }
+
+  const previousOffset = offset.value - limit.value
+  router.push(`?offset=${previousOffset}&limit=${limit.value}`)
+}
+
+const onNextPage = (e: Event) => {
+  e.preventDefault()
+
+  const nextOffset = offset.value + limit.value
+  router.push(`?offset=${nextOffset}&limit=${limit.value}`)
+}
 </script>
 
 <template>
@@ -20,6 +43,10 @@ const type = computed(() => route.params.type || null)
       </div>
       <TclFiltersSheet />
     </div>
-    <section class="mt-4 md:mt-8">contents</section>
+    <section class="mt-4 md:mt-8">
+      contents (offset: {{ offset }}, limit: {{ limit }}) |
+      <button :onClick="onPreviousPage">previous page</button> |
+      <button :onClick="onNextPage">next page</button>
+    </section>
   </div>
 </template>
