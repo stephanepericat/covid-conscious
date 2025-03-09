8884d70c06dc8bbb0ab9935f83ad3bf5c904248a
Stephane P. Pericat <contact@pericat.net>
2025-03-09 16:58:27 -0500

    fix(tcl-207): template error

--- pages/[type]/index.vue
+++ pages/[type]/index.vue
@@ -76,40 +76,6 @@ const total = computed(() => data?.value?.info?.total || 0)
         :total="total"
         :on-page-change="onPageChange"
       />
-        v-slot="{ page }"
-        :items-per-page="limit"
-        :total="total"
-        :sibling-count="1"
-        show-edges
-        :default-page="1"
-        class="flex justify-center"
-        @update:page="onPageChange"
-      >
-        <PaginationList v-slot="{ items }" class="flex items-center gap-1">
-          <PaginationFirst />
-          <PaginationPrev />
-
-          <template v-for="(item, index) in items">
-            <PaginationListItem
-              v-if="item.type === 'page'"
-              :key="index"
-              :value="item.value"
-              as-child
-            >
-              <Button
-                class="w-9 h-9 p-0"
-                :variant="item.value === page ? 'default' : 'outline'"
-              >
-                {{ item.value }}
-              </Button>
-            </PaginationListItem>
-            <PaginationEllipsis v-else :key="item.type" :index="index" />
-          </template>
-
-          <PaginationNext />
-          <PaginationLast />
-        </PaginationList>
-      </Pagination> -->
       <!-- <pre>{{ data }}</pre> -->
     </section>
   </div>
