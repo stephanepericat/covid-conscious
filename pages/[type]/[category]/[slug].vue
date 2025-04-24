6257e58a46a6e4dfcc491c62f06da57ab160fbf1
Stephane P. Pericat <contact@pericat.net>
2025-04-24 15:29:31 -0500

    feat(tcl-207): get article

--- pages/[type]/[category]/[slug].vue
+++ pages/[type]/[category]/[slug].vue
@@ -1,10 +1,27 @@
 <script lang="ts" setup>
+import PUBLICATION_QUERY from '@/sanity/queries/publication.sanity'
+import type { PUBLICATION_QUERYResult } from '@/sanity/types'
+
 const route = useRoute()
-const articleId = ref(route.params.slug)
+const { locale } = useI18n()
+
+const { category, slug, type } = route.params
+
+const { data, status } = await useLazySanityQuery<PUBLICATION_QUERYResult>(
+  PUBLICATION_QUERY,
+  { category, locale, slug, type },
+)
+
+const loading = computed(
+  () => status?.value === 'pending' || status?.value === 'idle',
+)
 </script>
 
 <template>
   <div class="container max-w-3xl py-4 md:py-8">
-    <h1>{{ articleId }}</h1>
+    <TclLoader v-if="loading" />
+    <template v-else>
+      <h1>{{ data?.title }}</h1>
+    </template>
   </div>
 </template>
