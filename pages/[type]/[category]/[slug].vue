3fa595f97c4fe435ef6394f4bd1353cc070ede40
Stephane P. Pericat <contact@pericat.net>
2025-04-24 14:28:30 -0500

    feat(tcl-207): article container

--- pages/[type]/[category]/[slug].vue
+++ pages/[type]/[category]/[slug].vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup>
 const route = useRoute()
+const articleId = ref(route.params.slug)
 </script>
 
 <template>
-  <h1>{{ route.params.slug }}</h1>
+  <div class="container max-w-3xl py-4 md:py-8">
+    <h1>{{ articleId }}</h1>
+  </div>
 </template>
